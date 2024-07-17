ed016656f1a94c08b2dc15a3957eb39cadcb2da0
Marcin Krasowski <mkrasowski@empik.com>
2024-07-18 01:19:13 +0200

    menu horizontal

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,14 +5,10 @@ export default function Header() {
     return <header>
         <div className="header-div">
             <h1 className="title"><Link to={PathConstants.HOME}>My React App</Link></h1>
-            <nav className="navbar">
-                <ul className="nav-list">
-                    <li className="nav-item" style={{float: "left"}}><Link to={PathConstants.HOME}>Home</Link></li>
-                    <li className="nav-item" style={{float: "left"}}><Link to={PathConstants.INGREDIENTS}>Ingredients</Link></li>
-                    <li className="nav-item" style={{float: "left"}}><Link to={PathConstants.RECIPES}>Recipes</Link></li>
-                    <li className="nav-item" style={{float: "left"}}><Link to={PathConstants.ABOUT}>About</Link></li>
-                </ul>
-            </nav>
+            <span style={{margin: "2px"}}><Link to={PathConstants.HOME}>Home</Link></span>
+            <span style={{margin: "2px"}}><Link to={PathConstants.INGREDIENTS}>Ingredients</Link></span>
+            <span style={{margin: "2px"}}><Link to={PathConstants.RECIPES}>Recipes</Link></span>
+            <span style={{margin: "2px"}}><Link to={PathConstants.ABOUT}>About</Link></span>
         </div>
     </header>
 }
