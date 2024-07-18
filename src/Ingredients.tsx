c741a26e551374380d8ce0a1127f983c32ebbda1
Marcin Krasowski <mkrasowski@empik.com>
2024-07-18 03:43:21 +0200

    add new name

--- src/Ingredients.tsx
+++ src/Ingredients.tsx
@@ -1,35 +1,63 @@
-import {Button, ListGroup, Modal, ModalDialog} from "react-bootstrap";
+import {Button, Form, ListGroup, Modal} from "react-bootstrap";
 import React, {useState} from "react";
+
+const ingredients = [
+    {"name": "Pepper", "unit": "g"},
+    {"name": "Salt", "unit": "g"},
+    {"name": "Milk", "unit": "ml"},
+    {"name": "Egg", "unit": "piece"},
+    {"name": "Olive Oil", "unit": "ml"},
+    {"name": "Garlic", "unit": "clove"}
+]
+
+interface FormElements extends HTMLFormControlsCollection {
+    newIngredientName: HTMLInputElement
+}
+
+interface AddNewIngredientFormElement {
+    readonly elements: FormElements
+}
 
 export default function() {
 
-    const ingredients = [
-        {"name": "Pepper", "unit": "g"},
-        {"name": "Salt", "unit": "g"},
-        {"name": "Sugar", "unit": "g"},
-        {"name": "Flour", "unit": "g"},
-        {"name": "Butter", "unit": "g"},
-        {"name": "Milk", "unit": "ml"},
-        {"name": "Egg", "unit": "piece"},
-        {"name": "Baking Powder", "unit": "g"},
-        {"name": "Olive Oil", "unit": "ml"},
-        {"name": "Garlic", "unit": "clove"}
-    ]
+    const [ingredientsList, setIngredientsList] = useState(ingredients)
 
-    function hideAddNew() {
-        setAddNewShown(false)
+    function addNewIngredient(e: React.FormEvent<AddNewIngredientFormElement>) {
+        e.preventDefault()
+        const name = e.currentTarget.elements.newIngredientName.value
+        ingredientsList.push({"name": name, unit: "g"})
+        setIngredientsList(ingredientsList)
+        console.log(ingredients)
+        hideAddNew()
     }
 
-    const AddNewModal = () => <Modal show={addNewShown}>
-        <Modal.Footer>
-            <Button onClick={hideAddNew}>Close</Button>
-        </Modal.Footer>
+    const AddNewModal = () => <Modal show={addNewShown} onHide={hideAddNew}>
+        <Modal.Header closeButton>
+            <Modal.Title>Add New</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={addNewIngredient}>
+            <Modal.Body>
+                <Form.Group controlId={"newIngredientName"}>
+                    <Form.Label>Add new ingredient</Form.Label>
+                    <Form.Control type={"text"} placeholder="Enter name" />
+                    {/*<Form.Text></Form.Text>*/}
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button type={"submit"}>Add</Button>
+                <Button onClick={hideAddNew}>Close</Button>
+            </Modal.Footer>
+        </Form>
     </Modal>
 
     const [addNewShown, setAddNewShown] = useState(false)
 
     function showAddNew() {
         setAddNewShown(true)
+    }
+
+    function hideAddNew() {
+        setAddNewShown(false)
     }
 
     return <>
